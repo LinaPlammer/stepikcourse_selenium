1e9b282d004c3e1576423c37f3f8c022ff99227d
Polina <polina@GISMETEO.RU>
2025-03-05 18:27:34 +0300

    тренируемся коммитить, добавляем новый файл в репозиторий

---
+++ 2.3newwindow.py
@@ -0,0 +1,35 @@
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/redirect_accept.html")
+    button = browser.find_element(By.CLASS_NAME, "trollface.btn.btn-primary")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-primary")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
